4a6a0f8a29958d6402fa88d554418ecfa66743ae
Clement Knox <clement@ems.ai>
2023-05-17 12:31:33 +0100

    cosmetic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>CONTAINER PAGE</h1>
+      <h1>Container Page</h1>
       <hr />
       <MarketingApp />
     </div>
